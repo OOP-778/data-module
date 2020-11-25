4e22592308b29402582fa64df77421a9e66224ce
Brian <oskardhavel@gmail.com>
2020-11-25 18:34:58 +0200

    remove exclude .ko

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,8 +53,6 @@ subprojects {
 
                 //relocate("org", "com.oop.datamodule.lib")
 
-                exclude("*.ko")
-
                 relocate("google", "com.oop.datamodule.lib")
                 relocate("com.google", "com.oop.datamodule.lib.google")
                 relocate("com.mysql", "com.oop.datamodule.lib.mysql")
