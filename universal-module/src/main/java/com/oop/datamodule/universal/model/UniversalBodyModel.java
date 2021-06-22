1d83b1237632536fdbd46cba5aab2419692fbe0d
oop-778 <oskardhavel@gmail.com>
2021-06-22 13:11:15 +0300

    Removed universal-module

--- universal-module/src/main/java/com/oop/datamodule/universal/model/UniversalBodyModel.java
+++
@@ -1,7 +0,0 @@
-package com.oop.datamodule.universal.model;
-
-import com.oop.datamodule.api.model.ModelBody;
-import com.oop.datamodule.commonsql.model.SqlModelBody;
-import com.oop.datamodule.mongodb.model.MongoModelBody;
-
-public interface UniversalBodyModel extends ModelBody, SqlModelBody, MongoModelBody {}
