1588dd6964bb3b445670feeaa311ce36cd3dbec8
oop-778 <oskardhavel@gmail.com>
2021-06-25 16:11:53 +0300

    More testing

--- property-module/src/main/java/com/oop/datamodule/property/Testing.java
+++ property-module/src/main/java/com/oop/datamodule/property/Testing.java
@@ -3,14 +3,10 @@ package com.oop.datamodule.property;
 import com.oop.datamodule.property.api.Property;
 import com.oop.datamodule.property.key.ImplKeyRegistry;
 
-import java.util.concurrent.TimeUnit;
-
 public class Testing {
   public static void main(String[] args) {
     ImplKeyRegistry registry = new ImplKeyRegistry(null);
 
     Property<Integer> a = null;
-    a
-            .getBlocking(TimeUnit.SECONDS, 10);
   }
 }
