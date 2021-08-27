6bcd18d6214a5ef7eeb74a95012b1fa4d1e8ba86
oop-778 <oskardhavel@gmail.com>
2021-08-27 10:59:55 +0300

    Fix for freezes on startup

--- api/src/main/java/com/oop/datamodule/api/util/job/JobsResult.java
+++ api/src/main/java/com/oop/datamodule/api/util/job/JobsResult.java
@@ -1,13 +1,16 @@
 package com.oop.datamodule.api.util.job;
 
+import java.util.Set;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class JobsResult {
-  private final List<Throwable> errors = new ArrayList<>();
-  private final AtomicInteger completed = new AtomicInteger(0);
+  private final Set<Throwable> errors;
+  private final AtomicInteger completed;
 }
--- api/src/main/java/com/oop/datamodule/api/util/job/JobsRunner.java
+++ api/src/main/java/com/oop/datamodule/api/util/job/JobsRunner.java
@@ -1,89 +1,45 @@
 package com.oop.datamodule.api.util.job;
 
-import com.oop.datamodule.api.StorageInitializer;
-import lombok.SneakyThrows;
-
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.SneakyThrows;
 
 public class JobsRunner {
-  private final Object lock = true;
+    private final Set<CompletableFuture<Void>> jobs = ConcurrentHashMap.newKeySet();
+    private final Set<Throwable> errors = ConcurrentHashMap.newKeySet();
 
-  private final Set<NativeJob> jobs = ConcurrentHashMap.newKeySet();
-  private final CompletableFuture<JobsResult> completionFuture = new CompletableFuture<>();
-  private JobsResult result;
-
-  protected JobsRunner() {}
-
-  public static JobsRunner acquire() {
-    return new JobsRunner();
-  }
-
-  @SneakyThrows
-  protected void onCompletion(NativeJob job) {
-    synchronized (lock) {
-      jobs.remove(job);
-
-      if (result == null) result = new JobsResult();
-
-      result.getCompleted().incrementAndGet();
-      if (job.resultedTo != null) result.getErrors().add(job.resultedTo);
-
-      if (jobs.isEmpty()) {
-        completionFuture.complete(result);
-      }
-    }
-  }
-
-  public JobsRunner addJob(Job job) {
-    NativeJob nativeJob = new NativeJob(job, this::onCompletion);
-    jobs.add(nativeJob);
-    return this;
-  }
-
-  public void startAndForget() {
-    for (NativeJob job : jobs) {
-      StorageInitializer.getInstance().getRunner(true).accept(job);
-    }
-  }
-
-  @SneakyThrows
-  public JobsResult startAndWait() {
-    if (jobs.isEmpty()) return new JobsResult();
-
-    for (NativeJob job : jobs) {
-      StorageInitializer.getInstance().getRunner(true).accept(job);
+    protected JobsRunner() {
     }
 
-    return completionFuture.get(3, TimeUnit.MINUTES);
-  }
-
-  protected static class NativeJob extends Job {
-    private final Job passed;
-    private final Consumer<NativeJob> completion;
-    private Throwable resultedTo;
-
-    public NativeJob(Job passed, Consumer<NativeJob> completion) {
-      this.passed = passed;
-      this.completion = completion;
+    public static JobsRunner acquire() {
+        return new JobsRunner();
     }
 
-    @Override
-    public String getName() {
-      return passed.getName();
+    public JobsRunner addJob(Job job) {
+        final CompletableFuture<Void> future = CompletableFuture
+            .runAsync(job);
+        future.whenComplete(($, error) -> {
+            if (error == null) {
+                return;
+            }
+            this.errors.add(error);
+        });
+        this.jobs.add(future);
+        return this;
     }
 
-    @Override
-    public void run() {
-      try {
-        passed.run();
-      } catch (Throwable throwable) {
-        resultedTo = throwable;
-      }
-      completion.accept(this);
+    @SneakyThrows
+    public JobsResult startAndWait() {
+        if (jobs.isEmpty()) {
+            return new JobsResult(errors, new AtomicInteger(0));
+        }
+
+        CompletableFuture
+            .allOf(jobs.toArray(new CompletableFuture[0]))
+            .get(1, TimeUnit.MINUTES);
+        return new JobsResult(errors, new AtomicInteger(jobs.size() - errors.size()));
     }
-  }
 }
